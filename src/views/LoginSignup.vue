782d2e44035ca199dd324c5d000fb622b990d74a
Emeka08 <ohaerifavour68@gmail.com>
2020-11-23 14:07:28 +0100

    working on the login form

--- src/views/LoginSignup.vue
+++ src/views/LoginSignup.vue
@@ -23,28 +23,72 @@
         <div class="modal-body">
           <div>
             <form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+              <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input
+                  type="email"
+                  class="form-control"
+                  id="exampleInputEmail1"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+                <small id="emailHelp" class="form-text text-muted"
+                  >We'll never share your email with anyone else.</small
+                >
+              </div>
+              <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input
+                  type="password"
+                  class="form-control"
+                  id="exampleInputPassword1"
+                  placeholder="Enter Password"
+                />
+              </div>
+              <div class="form-group form-check">
+                <input
+                  type="checkbox"
+                  class="form-check-input"
+                  id="exampleCheck1"
+                />
+                <label class="form-check-label" for="exampleCheck1"
+                  >Check me out</label
+                >
+              </div>
+              <div class="form-group">
+                <p class="text-center">
+                  By signing up you accept our <a href="#">Terms Of Use</a>
+                </p>
+              </div>
+              <div class="col-md-12 text-center">
+                <button
+                  type="submit"
+                  class="btn btn-block mybtn btn-primary tx-tfm"
+                >
+                  Login
+                </button>
+              </div>
+              <div class="col-md-12">
+                <div class="login-or">
+                  <hr class="hr-or" />
+                  <span class="span-or">or</span>
+                </div>
+              </div>
+              <div class="col-md-12 mb-3">
+                <p class="text-center">
+                  <a href="javascript:void();" class="google btn mybtn"
+                    ><i class="fa fa-google-plus"> </i> Signup using Google
+                  </a>
+                </p>
+              </div>
+              <div class="form-group">
+                <p class="text-center">
+                  Don't have account?
+                  <a href="#" id="signup" @click="swap">Sign up here</a>
+                </p>
+              </div>
+            </form>
           </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-dismiss="modal">
-            Close
-          </button>
-          <button type="button" class="btn btn-primary">Save changes</button>
         </div>
       </div>
     </div>
